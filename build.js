20d82e49412e725fbd61a556a4783e439021965d
Logan McAnsh <logan@mcan.sh>
2021-11-22 18:39:21 -0500

    chore: update build process
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- build.js
+++ build.js
@@ -1,25 +1,37 @@
 const path = require("path");
 
 const esbuild = require("esbuild");
+const fse = require("fs-extra");
 
 async function build() {
   let start = Date.now();
   console.log("BUILDING");
 
-  /**
-   * @type {import('esbuild').BuildOptions}
-   */
-  const default_options = {
+  await esbuild.build({
     entryPoints: ["server/index.js"],
     outfile: path.join(".output/server/pages", "index.js"),
     bundle: true,
     platform: "node",
-  };
-
-  await esbuild.build(default_options);
+  });
 
   let end = Date.now();
   console.log(`BUILT IN ${end - start}ms`);
+
+  ///////////////////////////////////////////////////////////////
+
+  start = Date.now();
+
+  console.log("COPYING FILES TO .OUTPUT");
+
+  await fse.mkdir(".output/static", { recursive: true });
+
+  await Promise.all([
+    fse.copy("public/build", ".output/static/build"),
+    fse.copyFile("routes-manifest.json", ".output/routes-manifest.json"),
+  ]);
+
+  end = Date.now();
+  console.log(`COPIED IN ${end - start}ms`);
 }
 
 build();
